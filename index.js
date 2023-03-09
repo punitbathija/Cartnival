0ed8d4b719cd5ef7827fa573a50c4d90c3ba671b
Punit Bathija <punit644@gmail.com>
2023-03-09 17:30:01 +0530

    Closed with error cannot read undefined tempFilePath

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 require("dotenv").config();
 const app = require("./app");
 const connectWithDB = require("./config/db");
+const cloudinary = require("cloudinary");
 
 // Connecting to db...
 
 connectWithDB();
+
+// Configuring cloudinary to handle assets
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`App is running at ${process.env.PORT}`);
