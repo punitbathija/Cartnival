acb40d0fa99e975117329fc8cfeb1922ce357cb7
Punit Bathija <punit644@gmail.com>
2023-03-13 16:35:12 +0530

    Added a middleware to check if user is signed in or not

--- routes/customer.js
+++ routes/customer.js
@@ -7,6 +7,7 @@ const {
   signout,
   forgotPassword,
   passwordReset,
+  getMyProfile,
 } = require("../controllers/customerController");
 
 router.route("/signup").post(signup);
@@ -14,5 +15,5 @@ router.route("/signin").post(signin);
 router.route("/signout").get(signout);
 router.route("/forgotpassword").post(forgotPassword);
 router.route("/password/reset/:token").post(passwordReset);
-
+router.route("/myprofile");
 module.exports = router;
