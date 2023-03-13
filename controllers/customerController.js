e115269f9c90c1e73c0b34b74134a6783b456a06
Punit Bathija <punit644@gmail.com>
2023-03-13 15:50:42 +0530

    Password reset route has some minor problem

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,9 +1,10 @@
 const Customer = require("../models/customer");
+const customer = require("../models/customer");
 const BigPromise = require("../middlewares/BigPromise");
 const CustomError = require("../utils/customError");
 const cookieToken = require("../utils/cookieToken");
-const customer = require("../models/customer");
 const mailHelper = require("../utils/mailHelper");
+const crypto = require("crypto");
 
 exports.signup = BigPromise(async (req, res, next) => {
   const { name, email, password } = req.body;
@@ -92,4 +93,37 @@ exports.forgotPassword = BigPromise(async (req, res, next) => {
 
     return next(new CustomError(error.message, 500));
   }
+});
+
+exports.passwordReset = BigPromise(async (req, res, next) => {
+  const token = req.params.token;
+
+  const encryptedToken = crypto
+    .createHash("sha256")
+    .update(token)
+    .digest("hex");
+
+  console.log(encryptedToken);
+
+  const customer = await Customer.findOne({
+    encryptedToken,
+    forgotPasswordExpiry: { $gt: Date.now() },
+  });
+
+  if (!customer) {
+    return next(new CustomError("Token is invalid or expired"));
+  }
+
+  if (req.body.password !== req.body.confirmPassword) {
+    return next(new CustomError("Password and confirm password do not match"));
+  }
+
+  customer.password = req.body.password;
+
+  customer.forgotPasswordToken = undefined;
+  customer.forgotPasswordExpiry = undefined;
+
+  await customer.save();
+
+  cookieToken(customer, res);
 });
