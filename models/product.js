bae6d9aebc71692aa2b2d406f0f31270ff0393f1
Punit Bathija <punit644@gmail.com>
2023-03-14 13:17:16 +0530

    Made a db model for products using mongoose

---
+++ models/product.js
@@ -0,0 +1,90 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter a product name"],
+    trim: true,
+    maxlength: [120, "Product name should not be more than 120 characters"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Please enter product price to continue"],
+    maxlength: [5, "Product cannot be more than 99999"],
+  },
+
+  description: {
+    type: String,
+    required: [true, "Please provide a description for the product"],
+  },
+
+  photos: {
+    id: {
+      type: String,
+      required: true,
+    },
+    secure_url: {
+      type: String,
+      required: true,
+    },
+  },
+
+  category: {
+    type: String,
+    required: [true, "Please choose a category to continue"],
+  },
+
+  brand: {
+    type: String,
+    required: [true, "Please add a brand to continue"],
+  },
+
+  quantity: {
+    type: Number,
+    required: [true, "Please enter the quantity to continue"],
+  },
+
+  ratings: {
+    type: Number,
+    default: 0,
+  },
+  numberOfReviews: {
+    type: Number,
+    default: 0,
+  },
+
+  reviews: [
+    {
+      customer: {
+        type: mongoose.Schema.ObjectId,
+        ref: "Customer",
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+      rating: {
+        type: Number,
+        required: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+
+  customer: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Customer",
+    required: true,
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Product", productSchema);
