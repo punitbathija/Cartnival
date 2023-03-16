6d5ce10a783c199de65c37664b3edeb46d3a4a6b
Punit Bathija <punit644@gmail.com>
2023-03-16 15:20:09 +0530

    Added a proxy to look up for server from the client

---
+++ .history/server/routes/product_20230314170528.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const {
+  productTest,
+  addProduct,
+  getAllProducts,
+  getSingleProduct,
+  adminModifyProduct,
+  adminDeleteProduct,
+  addReview,
+  deleteReview,
+  getReviewsForProduct,
+} = require("../controllers/productController");
+const { customRole, isLoggedin } = require("../middlewares/customer");
+
+router.route("/producttest").get(productTest);
+router.route("/products").get(getAllProducts);
+router.route("/product/:id").get(getSingleProduct);
+router.route("/review").post(isLoggedin, addReview);
+router.route("/review").delete(isLoggedin, deleteReview);
+router.route("/review").get(isLoggedin, getReviewsForProduct);
+
+// Admin Routes
+
+router
+  .route("/admin/product/add")
+  .post(isLoggedin, customRole("admin"), addProduct);
+router
+  .route("/admin/product/modify/:id")
+  .post(isLoggedin, customRole("admin"), adminModifyProduct);
+router
+  .route("/admin/product/delete/:id")
+  .delete(isLoggedin, customRole("admin"), adminDeleteProduct);
+
+module.exports = router;
