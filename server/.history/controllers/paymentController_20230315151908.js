928a4ec9e70827476fbf40689d6ce254f4490b86
Punit Bathija <punit644@gmail.com>
2023-03-16 15:05:44 +0530

    Moved all the files to the server directory

---
+++ server/.history/controllers/paymentController_20230315151908.js
@@ -0,0 +1,26 @@
+const Stripe = require("stripe");
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+const uuid = require("uuid");
+const BigPromise = require("../middlewares/BigPromise");
+
+exports.sendStripeKey = BigPromise(async (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    stripeKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  });
+});
+
+exports.captureStripePayment = BigPromise(async (req, res, next) => {
+  const charge = await stripe.charges.create({
+    amount: req.body.amount,
+    currency: "inr",
+  });
+
+  res.status(200).json({
+    success: true,
+    id: charge.id,
+    amount: charge.amount,
+    currency: charge.currency,
+    status: charge.status,
+  });
+});
