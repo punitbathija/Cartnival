fa8807f841514d1e7ff75c1222bc51e2df7d675a
Punit Bathija <punit644@gmail.com>
2023-03-22 15:15:18 +0530

    Successfully handled the update profile and password methods

--- .history/client/vite-project/src/pages/MyProfile_20230322150541.jsx
+++ .history/client/vite-project/src/pages/MyProfile_20230322150541.jsx
@@ -15,7 +15,7 @@ const MyProfile = () => {
         })
         .catch((error) => setError("Cannot fetch user details please sign in"));
     }
-    handleMyProfile();
+    // handleMyProfile();
   }, []);
 
   return (
@@ -33,33 +33,7 @@ const MyProfile = () => {
             Email:- {tokenData && <p>{tokenData.data.customer.email}</p>}
           </h1>
         )}
-        {tokenData && (
-          <button className="text-xl border-2 p-1.5 my-2 bg-cyan-700">
-            Update Password
-          </button>
-        )}
-
-        {tokenData && (
-          <div className="flex">
-            <button className=" flex text-xl border-2 p-1.5 my-2 bg-cyan-700">
-              Update User Details
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
-                />
-              </svg>
-            </button>
-          </div>
-        )}
+        {tokenData && <h1 className="flex md:text-xl">Update Password</h1>}
       </div>
     </div>
   );
--- .history/client/vite-project/src/pages/MyProfile_20230322151041.jsx
+++ .history/client/vite-project/src/pages/MyProfile_20230322151041.jsx
@@ -40,25 +40,23 @@ const MyProfile = () => {
         )}
 
         {tokenData && (
-          <div className="flex">
-            <button className=" flex text-xl border-2 p-1.5 my-2 bg-cyan-700">
-              Update User Details
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
-                />
-              </svg>
-            </button>
-          </div>
+          <button className="text-xl border-2 p-1.5 my-2 bg-cyan-700">
+            Update User Details{" "}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+              />
+            </svg>
+          </button>
         )}
       </div>
     </div>
