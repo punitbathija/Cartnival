0e1485c2df55a913ca7f17c7933fd3de767e49f7
Punit Bathija <punit644@gmail.com>
2023-03-31 17:11:56 +0530

    Working on the product page displaying all product info

--- client/vite-project/src/pages/ProductPage.jsx
+++ client/vite-project/src/pages/ProductPage.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import ProductSlider from "./productSlider";
 const api = import.meta.env.VITE_REACT_APP_BACKEND;
+import Rating from "@mui/material/Rating";
 
 const ProductPage = () => {
   const { id } = useParams();
@@ -23,8 +24,35 @@ const ProductPage = () => {
   }, []);
 
   return (
-    <div className="flex py-4 flex-wrap justify-center justify-items-center align-middle text-center m-auto dark:bg-neutral-800 dark:text-white ease-in duration-200 font-mono">
-      {productData && <ProductSlider productData={productData} />}
+    <div className="flex py-4 justify-center justify-items-center align-middle text-center m-auto dark:bg-neutral-800 dark:text-white ease-in duration-200 font-mono overflow-hidden">
+      {productData && (
+        <div className="border-2 md:flex">
+          <ProductSlider productData={productData} />
+          <div>
+            <h1 className="text-xl p-8 w-[500px] text-cyan-500 font-bold">
+              {productData.name}
+              <br />
+              <Rating name="read-only" value={productData.ratings} readOnly />
+              {productData.ratings && (
+                <p className="text-sm text-black dark:text-white">
+                  ({productData.reviews.length} ratings)
+                </p>
+              )}
+            </h1>
+            <p className="text-sm text-black dark:text-white">
+              In {productData.category}
+              <h1 className="text-3xl text-cyan-500">₹{productData.price}</h1>
+            </p>
+            <h1 className="text-md p-8 md:w-[500px] w-[350px] m-auto">
+              <span className="font-bold">About this item</span>
+              <br />
+              {productData.description}
+            </h1>
+            <button className="">Buy now</button>
+            <button>Add to cart</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
