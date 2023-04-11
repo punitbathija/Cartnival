868548ea3410c6f8f6cbdb85ae6bdf32eb49fffa
Punit Bathija <punit644@gmail.com>
2023-04-11 14:22:42 +0530

    Coming up with a solution to stay signed in even after redirect

--- client/vite-project/src/reduxStore.js
+++ client/vite-project/src/reduxStore.js
@@ -1,6 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
-import productReducer from "./productSlice";
 import cartReducer from "./cartSlice";
 
 export const store = configureStore({
