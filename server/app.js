efa902467894d60814cd7031e7f80c74102c1f79
Punit Bathija <punit644@gmail.com>
2023-04-20 18:35:53 +0530

    Another attempt to resloving routing issue

--- server/app.js
+++ server/app.js
@@ -1,24 +1,26 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const path = require('path');
 const app = express();
 const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
-
-
+const history = require("connect-history-api-fallback");
+const path = require("path");
 // Defining Middlewares
 
 const corsOptions = {
   origin: "https://cartnival.onrender.com",
 };
 
+// History Api
+app.use(history());
+
 // Express middlewares.
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'dist')));
-
+app.use(express.static("dist"));
+app.use(express.static(path.join(__dirname, "client", "dist")));
 
 // Cookies & file upload middlewares
 
@@ -43,10 +45,5 @@ app.use("/api/v1", product);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
 app.use("/api/v1", stripe);
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
-
 
 module.exports = app;
