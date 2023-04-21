fc1f18277b0909b4efc397a3474e7035adda7188
Punit Bathija <punit644@gmail.com>
2023-04-21 15:36:57 +0530

    Finally handled the payment failure page

--- client/src/pages/Failure.jsx
+++ client/src/pages/Failure.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Failure = () => {
+  const user = JSON.parse(localStorage.getItem("user"));
+
   return (
     <div className="px-4 py-6 flex-wrap justify-center justify-items-center align-middle text-center m-auto dark:bg-neutral-800 dark:text-white ease-in duration-200 font-mono overflow-hidden h-[100vh]">
-      <h1 className="md:text-xl text-lg ">
-        Uh no! something went wrong
+      <h1 className="md:text-xl text-lg text-red-600">
+        ❌ Oh no! something went wrong
         <span className="text-cyan-500"> {user.name} </span>
-        your Purchase was failed❎
+        your Purchase was failed ❌
       </h1>
       <br />
       <h1 className="md:text-lg">
